f3bdc6f07ba7fd857c625034b7426971bd34a442
Varun Adibhatla <vadibhatla@Varuns-MacBook-Air.local>
2025-12-12 15:31:52 +0530

    Switch to HashRouter for GitHub Pages support

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { HomeView } from './views/HomeView';
 import { RecorderView } from './views/RecorderView';
 import { PlayerView } from './views/PlayerView';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/record" element={<RecorderView />} />
         <Route path="/play/:id" element={<PlayerView />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
